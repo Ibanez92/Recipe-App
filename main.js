4dd1ec5ccb90e85f3aa3d0483346e0799c7d4f4a
Javier Ibanez <expertprep2015@gmail.com>
2021-02-21 01:39:42 -0500

    added api id and key.added view recipe button functionality to link to original recipe page.added calories, diet label, and health label using the recipe api names in the console log

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
-const searchForm = document.querySelector('form');
-const searchResultDiv = document.querySelector('.search-result');
-const container = document.querySelector('.container');
-let searchQuery = '';
+const searchForm = document.querySelector("form");
+const searchResultDiv = document.querySelector(".search-result");
+const container = document.querySelector(".container");
+let searchQuery = "";
+const APP_ID = "07fcefb4";
+const APP_key = "578eb3c979036e38033a02339474b8ba";
 
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    searchQuery = e.target.querySelector('input').value;
-    console.log(searchQuery);
-})
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector("input").value;
+  fetchAPI();
+});
+
+async function fetchAPI() {
+  const baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_key}&to=50`;
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  generateHTML(data.hits);
+  console.log(data);
+}
+function generateHTML(results) {
+  let generatedHTML = "";
+  results.map((result) => {
+    generatedHTML += 
+    `
+      <div class="item">
+       <img src="${result.recipe.image}" alt="">
+       <div class="flex-container">
+          <h1 class="title">${result.recipe.label}</h1>
+          <a class="view-button" href="${result.recipe.url}" target="_blank">View Recipe</a>
+       </div>
+       <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
+       <p class="item-data">Diet Label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : 'No Data Found'}</p>
+       <p class="item-data">Health Label: ${result.recipe.healthLabels}</p>
+      </div>
+      `;
+  });
+  searchResultDiv.innerHTML = generatedHTML;
+}
